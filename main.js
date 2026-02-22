1032495e764d7be7c6289ad6e462485a7ed255fa
Massao <tani22massao@gmail.com>
2026-02-22 18:54:47 +0900

    done

--- main.js
+++ main.js
@@ -43,10 +43,12 @@ var checkGridsFunction = function (board) {
   // Number of boxes inside the Grid 9x9
   for (let box = 0; box < 9; box++) {
     const seen = new Set();
+
+    // Calculate all rows x cols possible for the sub-grids.
     const row = Math.floor(box / 3) * 3;
     const col = (box % 3) * 3;
 
-    // Iterate through the grid 3x3
+    // Iterate through the sub-grids 3x3
     for (let i = 0; i < 3; i++) {
       for (let j = 0; j < 3; j++) {
         let current = board[row + i][col + j];
@@ -75,7 +77,7 @@ console.log(
   ]),
 );
 
-// False, There are number 8 in the same column (0) and grid (first 3x3)
+// False, There are more than one number 8 in the same column (0) and grid (first 3x3)
 console.log(
   isValidSudoku([
     ["8", "3", ".", ".", "7", ".", ".", ".", "."],
